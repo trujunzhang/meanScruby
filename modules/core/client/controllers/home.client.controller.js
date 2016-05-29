201fb8880bf6eb101b871aa2ead5e219ce475496
Trujun Zhang <trujunzhang@gmail.com>
2016-05-29 20:56:04 +0800

    mean.js for scruby.

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -5,7 +5,11 @@
         .module('core', ['duScroll'])
         .controller('HomeController', HomeController)
         .directive('ngRepeatOwlCarousel', repeatOwlCarousel)
-        .directive('faqContent', faqContent);
+        .filter("sanitize", ['$sce', function ($sce) {
+            return function (htmlCode) {
+                return $sce.trustAsHtml(htmlCode);
+            }
+        }]);
 
     HomeController.$inject = ['$scope', '$document'];
 
@@ -16,11 +20,33 @@
         vm.groups = [
             {
                 title: 'What is Scruby?',
-                content: 'Scruby is a smart downloader designed specifically for web crawling and scraping.It helps you to crawl the site quickly and reliably and reach your goals.'
+                content: '<p>Scruby is a smart downloader designed specifically for web crawling and scraping.It helps you to crawl the site quickly and reliably and reach your goals.</p>'
             },
             {
                 title: 'What tools, libraries or frameworks your site would be using for our project?',
-                content: 'Scrapy is an application framework for crawling web sites and extracting structured data which can be used for a wide range of useful applications, like data mining, information processing or historical archival.'
+                content: '<p>Scrapy is an application framework for crawling web sites and extracting structured data which can be used for a wide range of useful applications, like data mining, information processing or historical archival.</p>' +
+                '<p>This site scruby, built with ruby on rails. Support for exporting to excel automatically.</p>' +
+                '<p>We use mongo, nosql to store all crawled items, may scale well(ie. millions)</p>'
+            },
+            {
+                title: 'How can you prevent your Scrapy bot from getting banned?',
+                content: '<p>Some websites implement certain measures to prevent bots from crawling them, with varying degrees of sophistication.Getting around those measures can be difficult and tricky, and may sometimes require special infrastructure.</p>' +
+                '<p>We have IPs from 50+ countries available on request.</p>'
+            }, {
+                title: 'We need to crawl a site every week?',
+                content: '<p>Of course, No problem. We can crawl the site and send the data to your email every week.</p>'
+            }, {
+                title: 'Your price is too high?',
+                content: '<p>No. so inexpensive. Not like other paid website, Scruby is low price and high quality.</p>'
+            }, {
+                title: 'How do I contact you?',
+                content: '<p>If you need further assistance, please contact one of our customer service specialists:</p>' +
+                '<p>By email, at trujunzhang@gmail.com</p>' +
+                '<p>By clicking on the Contact Us link at the footer of homepage of our website.</p>'
+            }, {
+                title: 'Can I use  Scruby in my mobile phone?',
+                content: '<p>Yes, Scruby is a Responsive, Mobile-Friendly Website.</p>' +
+                '<p>You can use mobile web browser to browse it.</p>'
             }
         ];
 
@@ -55,14 +81,5 @@
         };
     }
 
-    function faqContent() {
-        return {
-            restrict: 'E',
-            scope: {
-                content: '@'
-            },
-            templateUrl: 'modules/core/client/views/faq.content.client.direcives.html'
-        };
-    }
 
 }());
