520bde6872dbdc4b968398360e5fa9023c87928c
Trujun Zhang <trujunzhang@gmail.com>
2016-05-31 17:45:36 +0800

    mean.js for scruby.

--- modules/itunes/client/controllers/list-itunes.client.controller.js
+++ modules/itunes/client/controllers/list-itunes.client.controller.js
@@ -10,10 +10,6 @@
     function ItunesListController(ItunesService, $stateParams, $http) {
         var vm = this;
 
-        // vm.page = $stateParams.page;
-        // vm.name = $stateParams.name;
-        // vm.itunes = ItunesService.query();
-
         vm.totalItems = 150;
         vm.currentPage = 1;
         vm.pageSize = 10;
@@ -23,16 +19,12 @@
         };
 
         vm.pageChanged = function () {
-            // page.number;
-            var page = vm.currentPage;
             vm.getItunes();
         };
 
         vm.getItunes = function () {
-
             $http.get('api/itunesList/' + vm.currentPage).success(function (response) {
                 vm.itunes = response;
-
             }).error(function (response) {
                 vm.error = response.message;
             });
@@ -41,10 +33,8 @@
         getTotalItems();
 
         function getTotalItems() {
-
             $http.get('api/totalItems/').success(function (response) {
                 vm.totalItems = response;
-
             }).error(function (response) {
                 vm.error = response.message;
             });
