976c1bf875f4842990c6a5c9a752ab0ecc844f13
Trujun Zhang <trujunzhang@gmail.com>
2016-05-31 14:23:51 +0800

    mean.js for scruby.

--- modules/itunes/client/controllers/list-itunes.client.controller.js
+++ modules/itunes/client/controllers/list-itunes.client.controller.js
@@ -14,8 +14,9 @@
         // vm.name = $stateParams.name;
         // vm.itunes = ItunesService.query();
 
-        vm.totalItems = 15;
+        vm.totalItems = 150;
         vm.currentPage = 1;
+        vm.pageSize = 10;
 
         vm.setPage = function (pageNo) {
             vm.currentPage = pageNo;
